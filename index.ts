b4bae6ab2065a5d18b3870f1d3d8f38b9024a635
maxmcd <max.t.mcdonnell@gmail.com>
2024-10-31 14:58:20 -0400

    in a branch

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export default async function(req: Request): Promise<Response> {
-    return new Response("Hello GitOps On Val Town");
+    return new Response("Hello GitOps On Val Town - in a branch");
 }
