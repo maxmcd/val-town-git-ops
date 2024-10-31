7399080eec3bb6e651086bb485145e505108a508
Max McDonnell <max.t.mcdonnell@gmail.com>
2024-10-31 14:20:37 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export default async function(req: Request): Promise<Response> {
-    return new Response("Hello GitOps On Val Town- on a branch?");
+    return new Response("Hello GitOps On Val Town - hi steve");
 }
