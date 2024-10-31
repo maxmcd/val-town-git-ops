dbe9ef6a0bc9ac913cd1c775b2291437c8133e2d
Max McDonnell <max.t.mcdonnell@gmail.com>
2024-10-31 15:28:25 -0400

    Something else

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export default async function(req: Request): Promise<Response> {
-    return new Response("Hello GitOps On Val Town - hi steve");
+    return new Response("Hello GitOps On Val Town, I want it to be this!");
 }
